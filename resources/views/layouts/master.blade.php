09691f20e4904c959c4b4524e1ea444d68c837c2
abdelaziz iddouch <abdelaziz.iddouch@gmail.com>
2018-05-09 23:09:33 +0100

    set up sass files and images , to my home page . (still need to import icons , and fix bugs , colors ...)

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,9 +4,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="./css/app.css">
         <title>Laravel</title>
-
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
